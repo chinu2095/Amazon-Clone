3345e09e06393fb58309ef269e547f3db7cd2a0f
Priyanshu Sharma <priyanshusharma2095@gmai.com>
2025-04-19 19:32:28 +0530

    using daysjs added delivery dates

---
+++ data/delivery.js
@@ -0,0 +1,13 @@
+export const deliveryOptions =[{
+  deliveryId:'1',
+  deliveryDays :7,
+  priceCents: 0
+},{
+  deliveryId:'2',
+  deliveryDays :3,
+  priceCents: 499
+},{
+  deliveryId:'3',
+  deliveryDays :1,
+  priceCents: 999
+}];
