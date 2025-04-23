a84777bd34659f05b3c1dea8e20ddd766c8c7d42
Priyanshu Sharma <priyanshusharma2095@gmai.com>
2025-04-23 19:28:12 +0530

    automated testing with jasmine

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -2,7 +2,7 @@ import{formatCurrency} from '../scripts/utils/money.js';
   
   console.log('Test Cases for: format currenct');
 
-   console.log('for cents to dollars'formatCurrency(2084) === '20.84'?'passed':'failed');
+   console.log(formatCurrency(2084) === '20.84'?'passed':'failed');
    console.log(formatCurrency(0) === '0.00'?'passed':'failed');
    console.log(formatCurrency(2000.5) === '20.01'?'passed':'failed');
    console.log(formatCurrency(1) === '0.01'?'passed':'failed');
