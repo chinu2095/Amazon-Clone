1f9be527b6c560d73a721fb9c841fb2e97c4bd33
Priyanshu Sharma <priyanshusharma2095@gmai.com>
2025-05-09 18:20:50 +0530

    moved to async await from promises , yehhhh

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,6 +6,30 @@ import{loadCart} from'../data/cart.js';
 //import '../data/cart-class.js';
 //import'../data/backend-practice.js';
 
+// async make a function returns a function
+async function loadPage(){
+
+  await loadProductsFetch();
+
+  await new promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+  renderCheckoutHeader();
+  // we can write asynchronous code like normal code
+  // async lets us uses await 
+  // AWAIt : lets us wait for the promise to finish before  going to the next step
+  return 'value2' ; // it will be stored in in teh next step (then); we can only use await in a async function
+
+}
+loadPage().then((value)=>{
+  console.log(value);
+
+})
 // promises helps avoiding nesting and make our code more redable its better to use promises instead of callbacks
 Promise.all([
   loadProductsFetch(),
