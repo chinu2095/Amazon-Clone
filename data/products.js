df7726a4e1aeacc2400d0aeecc58451f167833a2
Priyanshu Sharma <priyanshusharma2095@gmai.com>
2025-05-09 17:26:16 +0530

    learned promises and convert code to promises

--- data/products.js
+++ data/products.js
@@ -50,12 +50,31 @@ export function getProduct(productId){
     }
 
   }
+  export let products = [];
+   
+export function loadProducts(fun){
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load',()=>{
+    products = JSON.parse(xhr.response).map((productsDetails)=>{
+      if(productsDetails.type === "clothing"){
+        return new Clothing(productsDetails);
+      }
+       return new Product(productsDetails);
+    });
+    fun();
+    
+  })
+  xhr.open('GET','https://supersimplebackend.dev/products');
+  xhr.send();
+}
   
 
 
 
 
- export const products = [
+/*
+export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -720,3 +739,4 @@ export function getProduct(productId){
   }
    return new Product(productsDetails);
 });
+*/
